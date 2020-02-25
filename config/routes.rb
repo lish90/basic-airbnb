ae31c2ef91eff4a99c908ed80dfc4e48e7f0bd75
alisha musamabik <alisha.musamabik@gmail.com>
2020-02-25 20:08:27 +0000

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-
+  get "flats", to:"flats#index"
+  get "flats/new", to: "flats#new"
+  post "flats", to: "flats#create"
+  get  "flats/:id", to: "flats#show"
+  get "flats/:id/edit", to: "flats#edit"
+  patch "flats/:id", to: "flats#update"
+  delete "flats/:id", to:"flats#destory"
 end
