44c2ca1dcadf062a11e2dcdf611169ab227f6f62
alisha musamabik <alisha.musamabik@gmail.com>
2020-03-01 18:55:06 +0000

    adding controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before-action :set_flat, only: [:show, :edit, :update, :destory]
+    before_action :set_flat, only: [:show, :edit, :update, :destroy]
   
   def index
     @flats = Flat.all 
