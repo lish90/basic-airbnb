8a9d1ac1353ffefa5824144c71433371907dceed
alisha musamabik <alisha.musamabik@gmail.com>
2020-03-01 16:20:33 +0000

    adding controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,9 +1,29 @@
 class FlatsController < ApplicationController
   def index
   end 
-
+  
   def show
+  end
+
+  def new 
   end 
 
-  
+  def create
+  end 
+
+  def edit 
+  end 
+
+  def update
+  end 
+
+  def destory 
+  end 
+
+  Private 
+
+def set_flat
+@flat = Flat.find(parmas[:id])
+end 
+
 end
